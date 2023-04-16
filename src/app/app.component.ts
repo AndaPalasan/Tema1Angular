d646f3c0c6e024e13fd2e36f29c27c2235f86682
andagianinapalasan <anda-gianina.palasan@atos.net>
2023-04-16 20:08:35 +0300

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ChildrenOutletContexts } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'Tema1';
+
+  constructor(private contexts: ChildrenOutletContexts) { }
+
+}
+
